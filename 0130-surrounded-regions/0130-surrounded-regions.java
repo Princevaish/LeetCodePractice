8d542606a725d8208031856906681dc9857b7743
Prince vaish <118712855+Princevaish@users.noreply.github.com>
2026-08-02 20:13:33 +0530

    Time: 2 ms (72.59%), Space: 47.8 MB (58.28%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -0,0 +1,52 @@
+class Solution {
+    public void dfs(int row,int col,int [][] vis,char [][] board,int n,int m,int [] delrow,int [] delcol)
+    {
+        vis[row][col]=1;
+        for(int i=0;i<4;i++){
+            int nrow=row+delrow[i];
+            int ncol=col+delcol[i];
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]==0 && board[nrow][ncol]== 'O')
+            {
+                dfs(nrow,ncol,vis,board,n,m,delrow,delcol);
+            }
+        }
+    }
+    public void solve(char[][] board) {
+        int n=board.length;
+        int m=board[0].length;
+        int [][] vis= new int[n][m];
+        int [] delrow={-1,0,1,0};
+        int [] delcol={0,1,0,-1};
+        for(int i=0;i<m;i++)
+        {
+            if(board[0][i] == 'O' && vis[0][i]==0)
+            {
+                dfs(0,i,vis,board,n,m,delrow,delcol);
+            }
+            if(board[n-1][i] == 'O' && vis[n-1][i]==0)
+            {
+                dfs(n-1,i,vis,board,n,m,delrow,delcol);
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(board[i][0]=='O' && vis[i][0]==0)
+            {
+                dfs(i,0,vis,board,n,m,delrow,delcol);
+            }
+            if(board[i][m-1]=='O' && vis[i][m-1]==0)
+            {
+                dfs(i,m-1,vis,board,n,m,delrow,delcol);
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++)
+            {
+                if(board[i][j]=='O' && vis[i][j]==0)
+                {
+                    board[i][j]='X';
+                }
+            }
+        }
+    }
+}
