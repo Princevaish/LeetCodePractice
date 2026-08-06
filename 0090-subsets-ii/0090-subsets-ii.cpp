cacd9b2476e4780b2ef99336052f87c397b48109
Prince vaish <118712855+Princevaish@users.noreply.github.com>
2026-08-06 22:26:17 +0530

    Time: 0 ms (100%), Space: 10.3 MB (97.55%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+
+   void findSol(int ind, vector<int> &ds, vector<int> &nums,vector<vector<int>> &ans)
+   {
+     ans.push_back(ds);
+     for(int i=ind;i<nums.size();i++)
+     {
+        if(i!=ind && nums[i]==nums[i-1]) continue;
+        ds.push_back(nums[i]);
+        findSol(i+1,ds,nums,ans);
+        ds.pop_back();
+     }
+
+   }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> ds;
+        sort(nums.begin(),nums.end());
+        findSol(0,ds,nums,ans);
+        return ans;
+    }
+};
